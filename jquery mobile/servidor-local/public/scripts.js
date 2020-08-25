24dabab9b5ab287e0b93efd49ef38339cfc17ee9
Ing. Jose Alejandro Vargas <josevargu@gmail.com>
2020-08-25 17:45:32 -0500

    más ejemplos

--- jquery mobile/servidor-local/public/scripts.js
+++ jquery mobile/servidor-local/public/scripts.js
@@ -1,14 +1,22 @@
-$("#nosotros footer nav a").attr("style","background:blue"); 
+$("#nosotros footer nav a").attr("style", "background:blue");
 
 $("#chequear").bind("click", function () {
    $("#chequear input:checkbox").prop('checked') ? $("#nosotros footer li a").css("background", "yellow") : $("#nosotros footer li a").css("background", "red");
 })
 
-$("#grande").bind("click", ()=>{
-   $("#inicio p").css("font-size","2rem");
+$("#grande").bind("click", () => {
+   $("#inicio p").css("font-size", "2rem");
 })
 
-$("#pequeno").bind("click", ()=>{
-   $("#inicio p").css("font-size","1rem");
+$("#pequeno").bind("click", () => {
+   $("#inicio p").css("font-size", "1rem");
 })
 
+fetch('https://sserver.insoft.co/api/EsPrimo', {
+   method: "post",
+   headers: { Authorization: "Basic InSoftPruebas" },
+   body: JSON.stringify({ numero: 9973 }),
+})
+.then(data => data.json())
+.then(data => console.log(data))
+.catch(err => new Error("error en acceso"));
